ac921efb28355dc75ff292bb8755025c67a9cab6
yongbinlee <yblee001223@naver.com>
2023-06-09 13:32:03 +0900

    finish

---
+++ src/handdetection.py
@@ -0,0 +1,50 @@
+# mediapipe로 손가락 인식하기
+
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()  # 객체 생성
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    success, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            # mpDraw.draw_landmarks(frame, handLms) # 점만 찍힘
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm)
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                if id == 4:
+                    cv2.circle(frame, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(
+        frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3
+    )
+
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+# Path: src/mediapipe.py
